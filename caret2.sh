036b80ee01b328510653c97aedab0495a3b44302
st9540808 <st9540808@gmail.com>
2023-06-18 22:57:00 +0800

    Create `caret.sh` for awenv-caret-amd64:humble-230617

---
+++ caret2.sh
@@ -0,0 +1,6 @@
+# export ROS_LOCALHOST_ONLY=1
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export ROS_TRACE_DIR=~/ntu_ws/evaluate/
+source /home/st9540808/ros2_caret_ws_4.9/setenv_caret.bash
+source /home/st9540808/autoware_caret_humble/install/local_setup.bash
+export CYCLONEDDS_URI=file://$PWD/ntu_ws/cyclonedds.xml
